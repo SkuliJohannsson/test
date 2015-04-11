04db9c790513866f9bfd0a85b2e65d71f940c428
SkuliJohannsson <skjo20@cwno0461.work.enirogroup.com>
2015-04-11 23:56:03 +0200

    Minor changes

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ git commit -m "first commit"
 git remote add origin https://github.com/SkuliJohannsson/test.git
 git push -u origin master
 ```
+
+# Addon to change browser shortcuts in firefox
+
+You need to go to settings to disable the shortcuts you want to.
+
+https://addons.mozilla.org/en-US/firefox/addon/customizable-shortcuts/
--- src/learnshortcuts.js
+++ src/learnshortcuts.js
@@ -1,34 +1,40 @@
 $(function(){
 
-$.get("shortcuts/browse", function(data){
+$.get("shortcuts/Emacs", function(data){
     data = _.chain(data.split(/\r?\n/))
 	
 	.filter(function(line){ return !/^[ \t]*#/.test(line);})
 	.map(function(line){ 
 		var pars = line.split(/\t+/); 
-		return {keys:toCorrectKeys(pars[0]), message:pars[1].trim()}; 
+		return {keys:pars[0], message:pars[1].trim()}; 
 	})
 	.value();
     console.log(data);
 	
 	testKeyMap(data);
 	
-	function toCorrectKeys(keys){
-		return keys.toLowerCase().split(", ");
+	function toMousetrapFormat(keys){
+		return keys.toLowerCase()
+		.replace(/c[-+]/g,"ctrl+")
+		.replace(/m[-+]/g,"alt+")
+		.replace(/s[-+]/g,"shift+")
+		.replace(/spc/g,"space")
+		.split(", ");
 	}
 	
 	function testKeyMap(data){
 		var current = data.pop();
 		if(current)
 		{
-			console.log(current.message+'('+current.keys+')');
+			console.log(current.message+'('+toMousetrapFormat(current.keys+')'));
 			
-			Mousetrap.bind(current.keys, function(event){ 
+			Mousetrap.bind(toMousetrapFormat(current.keys), function(event, combo){ 
+				console.log(combo);
 				event.preventDefault();
 				Mousetrap.reset();
 				testKeyMap(data);
 			});
-			
+						
 			Mousetrap.bind('enter', function(event){ 
 				event.preventDefault();
 				console.log("Fail!!!!!!!!!!!!!");
