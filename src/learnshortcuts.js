721b4e27ba519255827e1014908736e91ed677c5
SkuliJohannsson <skjo20@cwno0461.work.enirogroup.com>
2015-04-12 00:20:05 +0200

    Now the website actuallys hows something.

--- src/learnshortcuts.js
+++ src/learnshortcuts.js
@@ -2,14 +2,12 @@ $(function(){
 
 $.get("shortcuts/Emacs", function(data){
     data = _.chain(data.split(/\r?\n/))
-	
 	.filter(function(line){ return !/^[ \t]*#/.test(line);})
 	.map(function(line){ 
 		var pars = line.split(/\t+/); 
 		return {keys:pars[0], message:pars[1].trim()}; 
 	})
 	.value();
-    console.log(data);
 	
 	testKeyMap(data);
 	
@@ -26,10 +24,10 @@ $.get("shortcuts/Emacs", function(data){
 		var current = data.pop();
 		if(current)
 		{
-			console.log(current.message+'('+toMousetrapFormat(current.keys+')'));
+			print(current.message);
 			
 			Mousetrap.bind(toMousetrapFormat(current.keys), function(event, combo){ 
-				console.log(combo);
+				printSuccess(current.keys);
 				event.preventDefault();
 				Mousetrap.reset();
 				testKeyMap(data);
@@ -37,15 +35,27 @@ $.get("shortcuts/Emacs", function(data){
 						
 			Mousetrap.bind('enter', function(event){ 
 				event.preventDefault();
-				console.log("Fail!!!!!!!!!!!!!");
+				printFail(current.keys);
 				Mousetrap.reset();
 				testKeyMap(data);
 			});
 		}
 		else
 		{
-			console.log("done");
+			print("done");
 		}
+	}
+	
+	function print(message){
+		$('.main').prepend("<div class='line'>"+message+"</div>");
+	}
+	
+	function printFail(message){
+		$( ".main .line" ).first().addClass( "fail" ).text(message);
+	}
+	
+	function printSuccess(message){
+		$( ".main .line" ).first().addClass( "success" ).text(message);
 	}
 });
 });
